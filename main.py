c9b406adc6e45ef6cc12c16ee16d8fe3a2df30b9
Antiajr <antiahope15@gmail.com>
2023-11-17 20:59:16 +0100

    data structure with class method

--- main.py
+++ main.py
@@ -75,3 +75,126 @@ for i in range(len(main_array)):
     for j in range(len(row)):
         sum += row[j]
 print(f'row of {row} = {sum}')
+
+stock_prices = {
+    'feb': 200,
+    'march': 300,
+    'april': 400,
+    'may': 500
+}
+stock_prices['june'] = stock_prices['feb'] + stock_prices['march']
+print(stock_prices)
+
+stock_data = [
+    {'feb': 200},
+    {'march': 300},
+    {'april': 400},
+    {'june': 500}
+]
+stock_data.append({'july':600})
+print(stock_data)
+# append into an empty list
+# lst1 = []
+# for i in range(5):
+#     lst1.append(i)
+# print(input('enter value: '))
+# print(lst1)
+
+stock_list = []
+stock_list.append(298)
+stock_list.append(305)
+stock_list.append(320)
+stock_list.insert(1,284)
+stock_list.insert(4, 400)
+print(stock_list)
+
+class Node:
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+class LimkedList:
+    def __init__(self):
+        self.head = None
+    
+    def insert_at_begininng(self, data):
+        node = Node(data, self.head)
+        self.head = node
+    
+    def print(self):
+        if self.head is None:
+            print("Linked list is empty")
+            return
+        itr = self.head
+        llstr = ''
+        
+        while itr:
+            llstr += str(itr.data) + '---->'
+            itr = itr.next
+            
+        print(llstr)
+    def insert_at_end(self, data):
+        if self.head is None:
+            self.head = None(data, None)
+            return
+        
+        itr = self.head
+        while itr.next:
+            itr = itr.next
+        itr.next = Node(data, None)
+        
+    def insert_value(self, data_list):
+        self.head = None
+        for data in data_list:
+            self.insert_at_end(data)
+    
+    def get_length(self):
+        count = 0
+        itr = self.head
+        while itr:
+            count +=1
+            itr = itr.next
+        return count
+    
+    def remove_at(self, index):
+        if index < 0 or index >= self.get_length():
+            raise Exception('invalid index')
+        
+        if index == 0:
+            self.head = self.head.next
+            return
+        
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                itr.next = itr.next.next
+                break
+            
+            itr = itr.next
+            count += 1
+    def insert_at(self, index, data):
+        if index < 0 or index > self.get_length():
+            raise Exception('invalid index')
+        
+        if index == 0:
+            self.insert_at_begininng(data)
+            return
+        count = 0
+        itr = self.head
+        while itr:
+            if count == index - 1:
+                node = Node(data, itr.next)
+                itr.next = node
+                break
+            itr = itr.next
+            count += 1
+            
+if __name__ == '__main__':
+    ll = LimkedList()
+    ll.insert_at_begininng(5)
+    ll.insert_at_begininng(89)
+    ll.insert_at_end(79)
+    ll.remove_at(2)
+    # ll.insert_value(['bannana', 'mango','grapes', 'orange'])
+    ll.print()
+    print('length: ', ll.get_length())
